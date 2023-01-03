8696cf3e10a49f235247487d8c47db200811c844
Airenas Vaičiūnas <airenass@gmail.com>
2023-01-03 14:45:44 +0200

    Print words recognition info

--- src/estimate.py
+++ src/estimate.py
@@ -1,8 +1,8 @@
 import argparse
 import sys
-from jiwer import compute_measures
 
 from evaluate import load
+from jiwer import compute_measures
 
 from src.normalizer import normalize
 
@@ -29,12 +29,18 @@ def main(argv):
     refs = read_file(args.ref)
     pred = read_file(args.pred)
 
+    print("Refs: {}, Predictions: {}".format(len(refs), len(pred)))
+    refs = refs[:len(pred)]
+
     wer = load("wer")
     wer_score = wer.compute(predictions=refs, references=pred)
     print("WER: {:.2f}".format(100 * wer_score))
 
     res = compute_measures(truth=refs, hypothesis=pred)
     print(res)
+    print("Err: {}/{}, (s: {}, d: {}, i: {})".format(res['substitutions'] + res['deletions'] + res['insertions'],
+                                                     res['hits'] + res['substitutions'] + res['deletions'],
+                                                     res['substitutions'], res['deletions'], res['insertions']))
 
 
 if __name__ == "__main__":
